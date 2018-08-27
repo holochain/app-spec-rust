1ac33f5dfa8272e1a0d297a3335bba97eec50adf
Nicolas Luck <nicolas@lucksus.eu>
2018-08-27 22:34:02 +0200

    Adjust to compile with changed HDK

--- zomes/blog/capabilities/main/code/src/lib.rs
+++ zomes/blog/capabilities/main/code/src/lib.rs
@@ -6,32 +6,42 @@ use std::time::SystemTime;
 
 #[no_mangle]
 pub extern "C" fn create_post(input: serde_json::Value) -> serde_json::Value {
-    let post_hash = hdk::commit("post", json!(
+    match hdk::commit_entry("post", json!(
         {
             "content": input["content"],
             "date_created": SystemTime::now()
         }
-    ));
+    )) {
+        Ok(post_hash) => {
+            hdk::link_entries(hdk::APP_AGENT_KEY_HASH.to_string(), post_hash.clone(), "authored_posts");
 
-    hdk::link(hdk::anchors::agent_key_hash(), "authored_posts", post_hash.clone());
+            let in_reply_to = input["in_reply_to"].to_string();
+            if in_reply_to != "" {
+                if let Ok(_) = hdk::get_entry(in_reply_to.clone()) {
+                    hdk::link_entries(in_reply_to, post_hash.clone(), "comments");
+                }
+            }
 
-    let in_reply_to = input["in_reply_to"].to_string();
-    if in_reply_to != "" {
-        if let Some(_) = hdk::get(in_reply_to.clone()) {
-            hdk::link(in_reply_to, "comments", post_hash.clone());
-        }
+            json!({"hash": post_hash})
+        },
+        Err(_) => json!({"error": "commit failed"})
     }
-
-    json!({"hash": post_hash})
 }
 
 #[no_mangle]
 pub extern "C" fn posts_by_agent(input: serde_json::Value) -> serde_json::Value {
-    let links = hdk::get_links(input["agent"].to_string(), "authored_posts");
-    json!({"post_hashes": links})
+    match hdk::get_links(input["agent"].to_string(), "authored_posts") {
+        Ok(links) => json!({"post_hashes": links}),
+        Err(hdk::ErrorCode::HashNotFound) => json!({"post_hashes": [], "error": "hash not found"}),
+        Err(hdk::ErrorCode::FunctionNotImplemented) => json!({"post_hashes": [], "error": "get_links not implemented yet!"}),
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn get_post(input: serde_json::Value) -> serde_json::Value {
-    json!({"post": hdk::get(input["post_hash"].to_string()) })
+    match hdk::get_entry(input["post_hash"].to_string()) {
+        Ok(entry) => json!({"post":  entry}),
+        Err(hdk::ErrorCode::HashNotFound) => json!({"post": {}, "error": "hash not found"}),
+        Err(hdk::ErrorCode::FunctionNotImplemented) => json!({"post": {}, "error": "get_entry not implemented yet!"}),
+    }
 }
