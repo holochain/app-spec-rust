f10bdab55a2a33789f20fdb1cc95cfabd462d77e
Nicolas Luck <nicolas@lucksus.eu>
2018-08-31 00:02:10 +0200

    Use zome_functions! macro

--- zomes/blog/capabilities/main/code/src/lib.rs
+++ zomes/blog/capabilities/main/code/src/lib.rs
@@ -1,45 +1,49 @@
+#[macro_use]
 extern crate hdk;
+extern crate holochain_wasm_utils;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
 use std::time::SystemTime;
 
-
-#[no_mangle]
-pub extern "C" fn create_post(input: serde_json::Value) -> serde_json::Value {
-    match hdk::commit_entry("post", json!(
-        {
-            "content": input["content"],
-            "date_created": SystemTime::now()
-        }
-    )) {
-        Ok(post_hash) => {
-            hdk::link_entries(hdk::APP_AGENT_KEY_HASH.to_string(), post_hash.clone(), "authored_posts");
-
-            let in_reply_to = input["in_reply_to"].to_string();
-            if in_reply_to != "" {
-                if let Ok(_) = hdk::get_entry(in_reply_to.clone()) {
-                    hdk::link_entries(in_reply_to, post_hash.clone(), "comments");
-                }
+zome_functions! {
+    create_post: |content: String, in_reply_to: String| {
+        match hdk::commit_entry("post", json!(
+            {
+                "content": content,
+                "date_created": "now"//SystemTime::now()
+                //SystemTime::now() panics when executed in wasmi
             }
+        )) {
+            Ok(post_hash) => {
+                hdk::link_entries(hdk::APP_AGENT_KEY_HASH.to_string(), post_hash.clone(), "authored_posts");
 
-            json!({"hash": post_hash})
-        },
-        Err(_) => json!({"error": "commit failed"})
+                let in_reply_to = in_reply_to;
+                if !in_reply_to.is_empty() {
+                    if let Ok(_) = hdk::get_entry(in_reply_to.clone()) {
+                        hdk::link_entries(in_reply_to, post_hash.clone(), "comments");
+                    }
+                }
+
+                json!({"hash": post_hash})
+            },
+            Err(_) => json!({"error": "commit failed"})
+        }
     }
-}
 
-#[no_mangle]
-pub extern "C" fn posts_by_agent(input: serde_json::Value) -> serde_json::Value {
-    match hdk::get_links(input["agent"].to_string(), "authored_posts") {
-        Ok(links) => json!({"post_hashes": links}),
-        Err(hdk_error) => hdk_error.to_json(),
+    posts_by_agent: |agent: String| {
+        match hdk::get_links(agent, "authored_posts") {
+            Ok(links) => json!({"post_hashes": links}),
+            Err(hdk_error) => hdk_error.to_json(),
+        }
     }
-}
 
-#[no_mangle]
-pub extern "C" fn get_post(input: serde_json::Value) -> serde_json::Value {
-    match hdk::get_entry(input["post_hash"].to_string()) {
-        Ok(entry) => json!({"post":  entry}),
-        Err(hdk_error) => hdk_error.to_json(),
+    get_post: |post_hash: String| {
+        match hdk::get_entry(post_hash) {
+            Ok(entry) => json!({"post":  entry}),
+            Err(hdk_error) => hdk_error.to_json(),
+        }
     }
 }
