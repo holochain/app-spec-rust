30f78ca1192202f11ae87e5df3da2a3dbb4c0377
Nicolas Luck <nicolas@lucksus.eu>
2018-08-31 19:36:13 +0200

    Clean up

--- test/test.js
+++ test/test.js
@@ -4,15 +4,12 @@ let app = Container.loadAndInstantiate("dist/bundle.json")
 app.start()
 
 test('create_post', function(t) {
-  //t.plan(2)
+  t.plan(1)
 
   let content = "Holo world"
   let in_reply_to = ""
   let params = JSON.stringify({content, in_reply_to})
-  console.log("params: " + params)
   let result = app.call("blog", "main", "create_post", params)
 
   t.equal(result, JSON.stringify({"error": "commit failed"}))
-
-  console.log("result: " + result)
 })
