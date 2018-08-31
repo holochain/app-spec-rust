8cc16b7d49f62ece73d6f6157b2cbcc0ff0cfd92
Nicolas Luck <nicolas@lucksus.eu>
2018-08-31 20:05:43 +0200

    Tests for all 3 zome functions

--- test/test.js
+++ test/test.js
@@ -1,15 +1,38 @@
-var test = require('tape');
+const test = require('tape');
 
-let app = Container.loadAndInstantiate("dist/bundle.json")
+const app = Container.loadAndInstantiate("dist/bundle.json")
 app.start()
 
-test('create_post', function(t) {
+test('create_post', (t) => {
   t.plan(1)
 
-  let content = "Holo world"
-  let in_reply_to = ""
-  let params = JSON.stringify({content, in_reply_to})
-  let result = app.call("blog", "main", "create_post", params)
+  const content = "Holo world"
+  const in_reply_to = ""
+  const params = JSON.stringify({content, in_reply_to})
+  const result = app.call("blog", "main", "create_post", params)
 
   t.equal(result, JSON.stringify({"error": "commit failed"}))
+})
+
+test('posts_by_agent', (t) => {
+  t.plan(1)
+
+  const agent = "Bob"
+  const params = JSON.stringify({agent})
+
+  const result = app.call("blog", "main", "posts_by_agent", params)
+
+  t.equal(result, JSON.stringify({"post_hashes": []}))
+})
+
+
+test('posts_by_agent', (t) => {
+  t.plan(1)
+
+  const post_hash = "abcd1234"
+  const params = JSON.stringify({post_hash})
+
+  const result = app.call("blog", "main", "get_post", params)
+
+  t.equal(result, JSON.stringify({"error": "Function not implemented"}))
 })
