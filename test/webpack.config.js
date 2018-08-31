da1042d36a99897f0b57f9773cff4f3b74bf99ca
Nicolas Luck <nicolas@lucksus.eu>
2018-08-31 20:03:12 +0200

    Configure Babel so we can write ES6 tests

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -6,6 +6,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env"]
+            }
+          }
+        }
+    ]
+  },
+  stats: {
+      colors: true
+  },
   node: {
     fs: 'empty',
     setImmediate: false
