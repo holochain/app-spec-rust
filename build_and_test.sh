c94c5e5f93f2eb31eb5c44a1e09dbe4268d818ef
Nicolas Luck <nicolas@lucksus.eu>
2018-09-29 11:35:21 +0200

    Echos for formatted CI output

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
 mkdir dist
+echo "===================================================================================="
+echo "BUILDING genome with 'hcdev package --output dist/app-spec-rust.hcpkg --strip-meta':"
+echo "------------------------------------------------------------------------------------"
 hcdev package --output dist/app-spec-rust.hcpkg --strip-meta
+echo "DONE."
+echo "===================================================================================="
+echo "BUILDING JS test file with webpack:"
+echo "------------------------------------------------------------------------------------"
 cd test
 npm install
 npm run build
 cd ..
+echo "DONE."
+echo "RUNNING tests with holoconsole..."
+echo "------------------------------------------------------------------------------------"
 holoconsole test/dist/bundle.js
+echo "DONE"
+echo "==================================================================================="
