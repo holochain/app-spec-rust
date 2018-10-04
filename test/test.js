c07d821e82ce25847e41fe29a8dc55f82528402f
Nicolas Luck <nicolas@lucksus.eu>
2018-10-04 16:17:14 -0400

    Expecting validation to trigger

--- test/test.js
+++ test/test.js
@@ -14,6 +14,17 @@ test('create_post', (t) => {
   t.equal(result, JSON.stringify({"hash":"Qma2xMsbBGp2baimoKhiZnMCzdcepvPzDXgktVrz3wQH8E"}))
 })
 
+test('post max content size 280 characters', (t) => {
+  t.plan(1)
+
+  const content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  const in_reply_to = ""
+  const params = JSON.stringify({content, in_reply_to})
+  const result = app.call("blog", "main", "create_post", params)
+
+  t.equal(result, JSON.stringify({"validationError":"Content too long"}))
+})
+
 test('posts_by_agent', (t) => {
   t.plan(1)
 
