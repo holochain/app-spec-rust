4d314d2e1a15e429f70e5eccf4e545e7df5cb2fd
Nicolas Luck <nicolas@lucksus.eu>
2018-10-03 20:40:23 -0400

    First go a validations! macro for entry and link validations, including setting of the validation package.

--- zomes/blog/code/src/post_validations.rs
+++ zomes/blog/code/src/post_validations.rs
@@ -1,35 +1,61 @@
-// THIS IS NOT FINAL
-// There are several things missing and not worked out yet.
-// Like:
-//   - validate_* functions need to receive a complex struct as argument
-//     that holds the entry to be validated next to the sources,
-//     validation package etc.
-//   - return type of these callbacks should be Result<..>, right?
-//   - how to declare/write validation packages?
-//   - do we need a guarding macro like zome_fns!?
-//
-// This file is WIP and placeholder to be overwritten by future
-// PR after completing the design discussion and reaching consensus
-// about these specifics.
 use serde_json;
 
+/// We declare the structure of our entry type with this Rust struct.
+/// It will be checked automatically by the macro below, similar
+/// to how this happens with functions parameters and zome_functions!.
 #[derive(Serialize, Deserialize)]
 struct Post {
     content: String,
-    date_created: String
+    date_created: String,
+    allow_comments: bool,
 }
 
-#[no_mangle]
-pub extern "C" fn validate_post(payload: String) -> bool {
-    serde_json::from_str::<Post>(&payload).is_ok()
-}
+/// We need a macro that wraps those validation functions similar to
+/// zome_functions!, but with different parameters.
+validations! {
 
-#[no_mangle]
-pub extern "C" fn validate_post_comments() -> bool {
-    true
-}
+    /// We also need to distinguish between the validation of an entry
+    /// (here) and validation of links (below)
+    [ENTRY] validate_post {
 
-#[no_mangle]
-pub extern "C" fn validate_post_authored_posts() -> bool {
-    true
+        /// This is an enum that needs to be added to the HDK and that tells
+        /// Holochain what validation package the source needs to send
+        /// to validating DHT nodes.
+        /// Should be something like:
+        ///
+        /// enum ValidationPackage {
+        ///    Entry,           //sending only the entry
+        ///    ChainEntries,    //sending all (public?) source chain entries
+        ///    ChainHeaders,    //sending all source chain headers
+        ///    ChainFull,       //sending the whole chain, entries and headers
+        ///    Custom(String),  //sending something custom
+        /// }
+        [ValidationPackage::ChainFull]
+
+        /// Here we have the validation function itself.
+        /// Deserializing the JSON happens behind the scenes and we get
+        /// a Post object.
+        /// ValidationData needs to be added to the HDK and should look
+        /// like this:
+        ///
+        /// struct ValidationData {
+        ///    sources : Vec<HashString>
+        ///    sourceChainEntries : Option<Vec<serde_json::Value>>
+        ///    sourceChainHeaders : Option<Vec<Header>>
+        ///    custom : Option<serde_json::Value>
+        /// }
+        |post: Post, ctx: hdk::ValidationData| {
+            post.content.len() < 280
+        }
+    }
+
+    /// Validation functions for links need to have a different signature:
+    [LINK] validate_post_comments: |base: HashString, target: HashString, ctx: hdk::ValidationData| {
+        let base = hdk::get_entry(base);
+        base.allow_comments
+    }
+
+    [LINK] validate_post_authored_posts: |base: HashString, target: HashString, ctx: hdk::ValidationData| {
+        ctx.sources[0] == base
+    }
 }
