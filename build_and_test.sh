16e1dc7c75630713a294949161dbb9c58fefbda7
Nicolas Luck <nicolas@lucksus.eu>
2018-10-03 22:19:58 -0400

    Pipe holoconsole output through faucet for pretty colors and the right return value

--- build_and_test.sh
+++ build_and_test.sh
@@ -15,4 +15,4 @@ cd ..
 echo "DONE."
 echo "RUNNING tests with holoconsole..."
 echo "------------------------------------------------------------------------------------"
-holoconsole test/dist/bundle.js
+holoconsole test/dist/bundle.js | test/node_modules/faucet/bin/cmd.js
