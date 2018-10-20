781775af2292a71fe171d1ff54958a5b53472699
Nicolas Luck <nicolas@lucksus.eu>
2018-10-20 22:40:35 +0200

    Comment

--- zomes/blog/code/src/lib.rs
+++ zomes/blog/code/src/lib.rs
@@ -13,6 +13,12 @@ pub mod post;
 
 use hdk::meta::ZomeDefinition;
 
+/// This function needs to be implemented by every zome.
+/// Every entry type needs to be registered by passing its
+/// definition object (constructed by entry!) to the define
+/// function.
+///
+/// Will soon be replaced by a define_zome! macro
 #[no_mangle]
 pub extern fn zome_setup(zd: &mut ZomeDefinition) {
     zd.define(post::definition());
